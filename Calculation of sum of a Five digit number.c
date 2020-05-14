fb830508bd8b4e1a34af656a9ab7e36aaeb92260
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-05-14 15:14:00 +0530

    Create Calculation of sum of a Five digit number.c

---
+++ Calculation of sum of a Five digit number.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a,b,c,d,e,sum,num;
+    
+    printf("Enter the five digit number\n");
+    scanf("%d",&n);
+    
+    
+a=n/10000;
+b=(n/1000)%10;
+c=(n/100)%10;
+d=(n/10)%10;
+e=n%10;
+    
+sum=a+b+c+d+e;
+    
+    printf("Sum of number=%d",sum);
+    
+    return 0;
+}
