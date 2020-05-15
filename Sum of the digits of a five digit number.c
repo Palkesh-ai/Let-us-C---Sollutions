2297ed6d4f4e727d17140196f9f877e620249aa6
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-05-15 15:24:18 +0530

    Create Sum of the digits of a five digit number.c

---
+++ Sum of the digits of a five digit number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int num,a,b,c,d,e,sum;
+
+    printf("Enter a five digit number");
+    scanf("%d",num);
+
+    a=num/10000;
+    b=(num/1000)%10;
+    c=(num/100)%10;
+    d=(num/10)%10;
+    e=num%10;
+    
+    sum=a+b+c+d+e;
+    
+    printf("The sum of every digit of the number is %d",sum);
+    
+    return 0;
+}
