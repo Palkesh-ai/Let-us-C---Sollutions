68b2ac8f4bcc633f898c2f1fc26cef42c7a40542
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-05-16 19:06:15 +0530

    Update Calculation of wind chill factor.c

--- Calculation of wind chill factor.c
+++ Calculation of wind chill factor.c
@@ -3,7 +3,20 @@
 
 int main()
 {
+    float wcf,t,v;
     
+    printf("***Enter the tempeteture of air***");
+    scanf("%f",&t);
+    
+    printf("***Enter the wind velocity***");
+    scanf("%f",&v);
+    
+    
+    wcf=35.74+0.6215t+(0.4275t-35.75)*pow(v,0.16);
+    
+    printf("The wind chill factor is %f",wcf);
+    
+    return 0;
 
 
 }
