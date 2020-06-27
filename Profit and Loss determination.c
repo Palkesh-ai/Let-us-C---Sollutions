57e3d57fa7949a808d68292aea5ef6abf36355e2
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-06-27 10:21:28 +0530

    Create Profit and Loss determination.c

---
+++ Profit and Loss determination.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()[
+ 
+ float cp,sp,profit,loss;
+ 
+ scanf("%f%f",&cp,&sp);
+ 
+ if(sp>cp);
+  printf("Profit=");
+ 
+ else 
+ 
+ if(sp==cp);
+  printf("Neither profit nor Loss");
+  
+ else
+ 
+ if(sp<cp);
+ 
+ printf("Loss");
+ 
+ 
+ return 0;
+
+]
+
