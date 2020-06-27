54b324ef742101945b9756e7b4bcbf316454f73f
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-06-27 14:52:59 +0530

    Create Odd and Even.c

---
+++ Odd and Even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()[
+
+int N,M;
+
+printf("Enter a number");
+scanf("%d",&M);
+
+N=M%2
+
+if(N==0);
+printf("It is an Even num.");
+
+else
+printf("It is Even");
+
+return 0;
+]
