d13291134bfee35a376b8b75308029fe246b34f6
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-06-27 10:23:52 +0530

    Update Profit and Loss determination.c

--- Profit and Loss determination.c
+++ Profit and Loss determination.c
@@ -6,18 +6,15 @@ int main()[
  scanf("%f%f",&cp,&sp);
  
  if(sp>cp);
-  printf("Profit=");
+     printf("Profit=");
  
  else 
- 
- if(sp==cp);
-  printf("Neither profit nor Loss");
+     if(sp==cp);
+     printf("Neither profit nor Loss");
   
  else
- 
- if(sp<cp);
- 
- printf("Loss");
+     if(sp<cp);
+     printf("Loss");
  
  
  return 0;
